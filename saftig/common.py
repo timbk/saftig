d737104642766f54f74a22cd83e958f2a24baa59
Tim Kuhlbusch <tim.kuhlbusch@rwth-aachen.de>
2025-06-19 18:17:05 +0200

    setup linter rules and clean up code accordingly

--- saftig/common.py
+++ saftig/common.py
@@ -1,3 +1,4 @@
+"""Shared functionality for all other modules"""
 from typing import Iterable, Union
 
 import numpy as np
@@ -6,7 +7,7 @@ def RMS(A:Iterable[float]):
     """ Calculate the root mean square value of an array """
     return np.sqrt(np.mean(np.square(A)))
 
-def make_2D_array(A:Iterable|Iterable[Iterable]):
+def make_2d_array(A:Iterable|Iterable[Iterable]):
     """add a dimension to 1D arrays and leave 2D arrays as they are
     This is intended to allow 1D array input for single channel application
 
@@ -16,10 +17,10 @@ def make_2D_array(A:Iterable|Iterable[Iterable]):
 
     :raises: ValueError if the input shape is not compatible
 
-    >>> make_2D_array([1, 2])
+    >>> make_2d_array([1, 2])
     array([[1, 2]])
 
-    >>> make_2D_array([[1, 2], [3, 4]])
+    >>> make_2d_array([[1, 2], [3, 4]])
     array([[1, 2],
            [3, 4]])
 
@@ -27,39 +28,45 @@ def make_2D_array(A:Iterable|Iterable[Iterable]):
     A = np.array(A)
     if len(A.shape) == 1:
         return np.array([A])
-    elif len(A.shape) == 2:
+    if len(A.shape) == 2:
         return A
-    else:
-        raise ValueError("Input must be 1D or 2D array")
+    raise ValueError("Input must be 1D or 2D array")
 
 
 class FilterBase:
     """ common interface definition for Filter implementations
 
-    :param N_filter: Length of the FIR filter (how many samples are in the input window per output sample)
+    :param n_filter: Length of the FIR filter
+                     (how many samples are in the input window per output sample)
     :param idx_target: Position of the prediction
-    :param N_channel: Number of witness sensor channels
+    :param n_channel: Number of witness sensor channels
     """
     filter_state = None
 
-    def __init__(self, N_filter:int, idx_target:int, N_channel:int=1):
-        self.N_filter = N_filter
-        self.N_channel = N_channel
+    def __init__(self, n_filter:int, idx_target:int, n_channel:int=1):
+        self.n_filter = n_filter
+        self.n_channel = n_channel
         self.idx_target = idx_target
 
-        assert self.N_filter > 0, "N_filter must be a positive integer"
-        assert self.N_channel > 0, "N_filter must be a positive integer"
-        assert self.idx_target >= 0 and self.idx_target < self.N_filter, "idx_target must not be negative and smaller than N_filter"
+        assert self.n_filter > 0, "n_filter must be a positive integer"
+        assert self.n_channel > 0, "n_filter must be a positive integer"
+        assert self.idx_target >= 0 and self.idx_target < self.n_filter, \
+                "idx_target must not be negative and smaller than n_filter"
 
-    def condition(self, witness:Iterable[float]|Iterable[Iterable[float]], target:Iterable[float]):
+    def condition(self,
+                  witness:Iterable[float]|Iterable[Iterable[float]],
+                  target:Iterable[float]):
         """ Use an input dataset to condition the filter
         
         :param witness: Witness sensor data
         :param target: Target sensor data
         """
-        pass # this should be implemented by the child class
+        # this should be implemented by the child class
 
-    def apply(self, witness:Iterable[float]|Iterable[Iterable[float]], target:Iterable[float], pad:bool=True, update_state:bool=False):
+    def apply(self,
+              witness:Iterable[float]|Iterable[Iterable[float]],
+              target:Iterable[float], pad:bool=True,
+              update_state:bool=False):
         """ Apply the filter to input data
         
         :param witness: Witness sensor data
@@ -69,10 +76,12 @@ class FilterBase:
 
         :return: prediction
         """
-        pass # this should be implemented by the child class
+        # this should be implemented by the child class
 
-    def check_data_dimensions(self, witness:Iterable[float]|Iterable[Iterable[float]], target:Iterable[float]) -> Union[Iterable[Iterable[float]], Iterable[float]]:
-        """Check the dimensions of the provided input data and apply make_2D_array()
+    def check_data_dimensions(self,
+                              witness:Iterable[float]|Iterable[Iterable[float]],
+                              target:Iterable[float]) -> Union[Iterable[Iterable[float]], Iterable[float]]:
+        """Check the dimensions of the provided input data and apply make_2d_array()
 
         :param witness: Witness sensor data
         :param target: Target sensor data
@@ -81,9 +90,8 @@ class FilterBase:
 
         :raises: AssertionError
         """
-        witness = make_2D_array(witness)
-        assert witness.shape[0] == self.N_channel, "witness data shape does not match configured channel count"
+        witness = make_2d_array(witness)
+        assert witness.shape[0] == self.n_channel, "witness data shape does not match configured channel count"
         assert target is None or target.shape[0] == witness.shape[1], "Missmatch between target and witness data shapes"
 
         return witness, target
-
--- saftig/wf.py
+++ saftig/wf.py
@@ -1,57 +1,57 @@
+""" Clasical static Wiener filter """
 from typing import Union, Iterable
 
 import numpy as np
 from scipy.signal import correlate
-import scipy
 
-from .common import FilterBase, make_2D_array
-
-from icecream import ic
-
+from .common import FilterBase, make_2d_array
 
 def mean_cross_correlation_offset(A, B, N, offset):
     """ estimate the cross-correlation between A and B """
     assert len(A) == len(B)
+
     if offset < N-1:
         return correlate(A, B[offset:-N+1+offset], mode='valid')
-    else:
-        return correlate(A, B[offset:], mode='valid')
+    return correlate(A, B[offset:], mode='valid')
 
-def wf_calculate(witness:Iterable[float]|Iterable[Iterable[float]], target:Iterable[float], N_filter:int, idx_target:int=0) -> Union[Iterable[Iterable[float]],bool]:
+def wf_calculate(witness:Iterable[float]|Iterable[Iterable[float]],
+                 target:Iterable[float],
+                 n_filter:int,
+                 idx_target:int=0) -> Union[Iterable[Iterable[float]],bool]:
     """ caluclate the FIR coefficients for a wiener filter
 
     :param witness: Witness sensor data
     :param witness: Target sensor data
-    :param N_filter: Length of the FIR filter (how many samples are in the input window per output sample)
+    :param n_filter: Length of the FIR filter (how many samples are in the input window per output sample)
     :param idx_target: offset of the prediction relative to the end of the array
 
     :return: filter coefficients, full_rank (bool)
     """
     target = np.array(target)
-    witness = make_2D_array(witness)
+    witness = make_2d_array(witness)
     assert witness.shape[1] == target.shape[0], "Missmatch between witness and target data shape"
-    assert N_filter <= target.shape[0], "Input data must be at least as long as the filter"
+    assert n_filter <= target.shape[0], "Input data must be at least as long as the filter"
 
     # calculate input autocorrelation and cross-correlation to target
-    R_ws = [mean_cross_correlation_offset(target, A, N_filter, idx_target) for A in witness] # R_ws[channel, time]
+    R_ws = [mean_cross_correlation_offset(target, A, n_filter, idx_target) for A in witness] # R_ws[channel, time]
     R_ws = np.array(R_ws).flatten(order='C')
 
-    def calc_r_matrix(A, B, N_filter):
+    def calc_r_matrix(A, B, n_filter):
         """ calculate the cross correlation matrix of a and b """
-        cc = correlate(A, B[:-N_filter], mode='valid')
-        return np.array([np.concatenate([cc[i::-1], cc[1:N_filter-i]]) for i in range(N_filter)])
-    def calc_r_matrix_symmetric(A, B, N_filter):
+        cc = correlate(A, B[:-n_filter], mode='valid')
+        return np.array([np.concatenate([cc[i::-1], cc[1:n_filter-i]]) for i in range(n_filter)])
+    def calc_r_matrix_symmetric(A, B, n_filter):
         """ calculate the cross correlation matrix of a and b and average positive and negative lag
             to make the result symmetric (as is expected for an autocorrelation)
         """
-        cc = correlate(A, B[N_filter:-N_filter], mode='valid')
-        cc = np.concatenate([ [cc[N_filter]], (cc[N_filter+1:] + cc[N_filter-1::-1])/2 ])
-        return np.array([np.concatenate([cc[i::-1], cc[1:N_filter-i]]) for i in range(N_filter)])
+        cc = correlate(A, B[n_filter:-n_filter], mode='valid')
+        cc = np.concatenate([ [cc[n_filter]], (cc[n_filter+1:] + cc[n_filter-1::-1])/2 ])
+        return np.array([np.concatenate([cc[i::-1], cc[1:n_filter-i]]) for i in range(n_filter)])
 
-    if len(target) >= 3*N_filter: # using both sides is only possible if enugh data is provided
-        R_ww = np.block([[calc_r_matrix_symmetric(A, B, N_filter) for B in witness] for A in witness])
+    if len(target) >= 3*n_filter: # using both sides is only possible if enugh data is provided
+        R_ww = np.block([[calc_r_matrix_symmetric(A, B, n_filter) for B in witness] for A in witness])
     else:
-        R_ww = np.block([[calc_r_matrix(A, B, N_filter) for B in witness] for A in witness])
+        R_ww = np.block([[calc_r_matrix(A, B, n_filter) for B in witness] for A in witness])
 
     # calculate pseudo-inverse correlation matrix of inputs and the filter coefficients
     # for some reason the scipy.linalg implementations were extremely slow on white noise test case => using numpy
@@ -60,7 +60,7 @@ def wf_calculate(witness:Iterable[float]|Iterable[Iterable[float]], target:Itera
     WFC = R_ww_inv.dot(R_ws)
 
     # unwrap into seperate FIR filters
-    WFC = WFC.reshape((len(witness), N_filter))
+    WFC = WFC.reshape((len(witness), n_filter))
     WFC = np.array([np.flip(i) for i in WFC])
     return WFC, full_rank
 
@@ -78,14 +78,14 @@ def wf_apply(WFC:Iterable[Iterable[float]], witness:Iterable[Iterable[float]]) -
 class WienerFilter(FilterBase):
     """Satic Wiener filter implementation
 
-    :param N_filter: Length of the FIR filter (how many samples are in the input window per output sample)
+    :param n_filter: Length of the FIR filter (how many samples are in the input window per output sample)
     :param idx_target: Position of the prediction
-    :param N_channel: Number of witness sensor channels
+    :param n_channel: Number of witness sensor channels
 
     >>> import saftig as sg
-    >>> N_filter = 128
+    >>> n_filter = 128
     >>> witness, target = sg.TestDataGenerator(0.1).generate(int(1e5))
-    >>> filt = sg.WienerFilter(N_filter, 0, 1)
+    >>> filt = sg.WienerFilter(n_filter, 0, 1)
     >>> _coefficients, full_rank = filt.condition(witness, target)
     >>> full_rank
     True
@@ -99,10 +99,9 @@ class WienerFilter(FilterBase):
     #: The FIR coefficients of the WF
     filter_state:Iterable[Iterable[float]]|None = None
 
-    def __init__(self, N_filter:int, idx_target:int, N_channel:int=1):
-        super().__init__(N_filter, idx_target, N_channel)
-
-    def condition(self, witness:Iterable[float]|Iterable[Iterable[float]], target:Iterable[float]) -> bool:
+    def condition(self,
+                  witness:Iterable[float]|Iterable[Iterable[float]],
+                  target:Iterable[float]) -> bool:
         """ Use an input dataset to condition the filter
 
         :param witness: Witness sensor data
@@ -110,13 +109,20 @@ class WienerFilter(FilterBase):
         """
         witness, target = self.check_data_dimensions(witness, target)
 
-        self.filter_state, full_rank = wf_calculate(witness, target, self.N_filter, idx_target=self.idx_target)
+        self.filter_state, full_rank = wf_calculate(witness,
+                                                    target,
+                                                    self.n_filter,
+                                                    idx_target=self.idx_target)
 
         if not full_rank:
             print("Warning: Filter is not of full rank")
         return self.filter_state, full_rank
 
-    def apply(self, witness:Iterable[float]|Iterable[Iterable[float]], target:Iterable[float]=None, pad:bool=True, update_state:bool=False) -> Iterable[float]:
+    def apply(self,
+              witness:Iterable[float]|Iterable[Iterable[float]],
+              target:Iterable[float]=None,
+              pad:bool=True,
+              update_state:bool=False) -> Iterable[float]:
         """ Apply the filter to input data
 
         :param witness: Witness sensor data
@@ -131,6 +137,5 @@ class WienerFilter(FilterBase):
 
         pred = wf_apply(self.filter_state, witness)
         if pad:
-            pred = np.concatenate([np.zeros(self.N_filter-1-self.idx_target), pred, np.zeros(self.idx_target)])
+            pred = np.concatenate([np.zeros(self.n_filter-1-self.idx_target), pred, np.zeros(self.idx_target)])
         return pred
-
