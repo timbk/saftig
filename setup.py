a0bec5ed8513e07a60d94e990bbe27fa00f0022b
Tim Kuhlbusch <tim.kuhlbusch@rwth-aachen.de>
2025-06-24 11:57:16 +0200

    adjust setup.py/pyproject.toml to make the CI tests happy

--- setup.py
+++ setup.py
@@ -7,13 +7,6 @@ c_modules = [
 ]
 
 setup(
-        name="saftig",
-        version="0.1",
-        description="Collection of implementations for noise prediction techniques like the Wiener filter",
-        url="https://github.com/timbk/saftig",
-        author="Tim Kuhlbusch et al.",
-        author_email="kuhlbusch@physik.rwth-aachen.de",
-
         install_requires=[
                           "numpy",
                           "matplotlib",
