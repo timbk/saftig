df4d9e8b114074544971ee1401217ccaaa489402
Tim Kuhlbusch <tim.kuhlbusch@rwth-aachen.de>
2025-06-24 15:31:08 +0200

    switch away from the separate C++ class for the LMS It has to be much more integrated with the python loops and cannot be standalone

--- saftig/_lms_c.cpp
+++ saftig/_lms_c.cpp
@@ -9,53 +9,91 @@
 #include <vector>
 #include <iostream>
 
-/// LeastMeanSquares implementation
-class LMSFilter{
-private:
-    uint32_t n_filter, idx_target, n_channel;
-    float step_scale;
-    bool normalized;
-
-    std::vector<std::vector<double>> filter_coefficients;
-public:
-    LMSFilter(uint32_t n_filter,
-            uint32_t idx_target,
-            uint32_t n_channel,
-            float step_scale,
-            bool normalized=true)
-        : n_filter(n_filter),
-          idx_target(idx_target),
-          n_channel(n_channel),
-          step_scale(step_scale),
-          normalized(normalized),
-          filter_coefficients(n_channel, std::vector<double>(n_filter, 0)) {
-    }
-
-    /**
-     * @brief reset filter parameters
-     */
-    void reset() {
-        for(auto channel: filter_coefficients) {
-            std::fill(channel.begin(), channel.end(), 0.);
-        }
-    }
-
-    uint32_t get_n_filter() {return n_filter;}
-    uint32_t get_idx_target() {return idx_target;}
-    uint32_t get_n_channel() {return n_channel;}
-
-    double step() {
-        return 0.;
-    }
-};
+// /// LeastMeanSquares implementation
+// class LMSFilter{
+// private:
+//     uint32_t n_filter, idx_target, n_channel;
+//     float step_scale;
+//     bool normalized;
+// 
+//     std::vector<std::vector<double>> filter_coefficients;
+// public:
+//     LMSFilter(uint32_t n_filter,
+//             uint32_t idx_target,
+//             uint32_t n_channel,
+//             float step_scale,
+//             bool normalized=true)
+//         : n_filter(n_filter),
+//           idx_target(idx_target),
+//           n_channel(n_channel),
+//           step_scale(step_scale),
+//           normalized(normalized),
+//           filter_coefficients(n_channel, std::vector<double>(n_filter, 0)) {
+//     }
+// 
+//     /**
+//      * @brief reset filter parameters
+//      */
+//     void reset() {
+//         for(auto channel: filter_coefficients) {
+//             std::fill(channel.begin(), channel.end(), 0.);
+//         }
+//     }
+// 
+//     uint32_t get_n_filter() {return n_filter;}
+//     uint32_t get_idx_target() {return idx_target;}
+//     uint32_t get_n_channel() {return n_channel;}
+// 
+//     double step() {
+//         return 0.;
+//     }
+// };
 
 
 
 typedef struct {
     PyObject_HEAD;
-    LMSFilter *filter;
+    int n_filter, idx_target, n_channel;
+    float step_scale;
+    bool normalized;
+
+    std::vector<std::vector<double>> filter_coefficients;
     /* Type-specific fields go here. */
 } LMS_C_OBJECT;
+
+/**
+ * check that the array is 2D with the given shape and dtype double
+ * raise an exception if not
+ * returns false if an exception was raised
+ */
+bool check_array_properties(PyArrayObject *array, int shape0, int shape1) {
+    // check dtype
+    if(PyArray_TYPE(array) != NPY_FLOAT64) {
+        PyErr_SetString(PyExc_ValueError, "np.float64 is the only supported dtype");
+        return false;
+    }
+
+    // Check that it's a 2D array
+    if (PyArray_NDIM(array) != 2) {
+        std::cout << "2+" << std::endl;
+        PyErr_SetString(PyExc_ValueError, "Input must be a 2D array.");
+        return false;
+    }
+
+    // check that dimensions match
+    npy_intp* dims = PyArray_DIMS(array);
+    int channels = dims[0], n_filter = dims[1];
+
+    if(channels != shape0) {
+        PyErr_SetString(PyExc_ValueError, "Input channel count missmatch");
+        return false;
+    }
+    if(n_filter != shape1) {
+        PyErr_SetString(PyExc_ValueError, "Input sample count missmatch");
+        return false;
+    }
+    return true;
+}
 
 static PyObject *
 LMS_C_step(LMS_C_OBJECT *self, PyObject *args)
@@ -72,29 +110,7 @@ LMS_C_step(LMS_C_OBJECT *self, PyObject *args)
         return NULL;
     }
 
-    // check dtype
-    if(PyArray_TYPE(array) != NPY_FLOAT64) {
-        PyErr_SetString(PyExc_ValueError, "np.float64 is the only supported dtype");
-        return NULL;
-    }
-
-    // Check that it's a 2D array
-    if (PyArray_NDIM(array) != 2) {
-        std::cout << "2+" << std::endl;
-        PyErr_SetString(PyExc_ValueError, "Input must be a 2D array.");
-        return NULL;
-    }
-
-    // check that dimensions match
-    npy_intp* dims = PyArray_DIMS(array);
-    int channels = dims[0], n_filter = dims[1];
-
-    if(channels != self->filter->get_n_channel()) {
-        PyErr_SetString(PyExc_ValueError, "Input channel count missmatch");
-        return NULL;
-    }
-    if(n_filter != self->filter->get_n_filter()) {
-        PyErr_SetString(PyExc_ValueError, "Input sample count missmatch");
+    if(not check_array_properties(array, self->n_channel, self->n_filter)) {
         return NULL;
     }
 
@@ -108,30 +124,45 @@ LMS_C_step(LMS_C_OBJECT *self, PyObject *args)
         return NULL;
     }
 
-    // loop
-    if (NpyIter_GetIterSize(iter) != 0) {
-        iternext = NpyIter_GetIterNext(iter, NULL);
-        dataptr = (double**)NpyIter_GetDataPtrArray(iter);
-        NpyIter_GetMultiIndexFunc *get_multi_index = NpyIter_GetGetMultiIndex(iter, NULL);
-
-        if ((iternext == NULL) || (dataptr == NULL) || (get_multi_index == NULL)) {
-            NpyIter_Deallocate(iter);
-            return NULL;
-        }
-
-        do {
-            get_multi_index(iter, multi_index);
-            printf("multi_index is [%" NPY_INTP_FMT ", %" NPY_INTP_FMT "], %lf\n",
-                   multi_index[0], multi_index[1], **dataptr);
-        } while (iternext(iter));
+    if (NpyIter_GetIterSize(iter) == 0) {
+        NpyIter_Deallocate(iter);
+        return NULL;
     }
+
+    iternext = NpyIter_GetIterNext(iter, NULL);
+    dataptr = (double**)NpyIter_GetDataPtrArray(iter);
+    get_multi_index = NpyIter_GetGetMultiIndex(iter, NULL);
+
+    if ((iternext == NULL) || (dataptr == NULL) || (get_multi_index == NULL)) {
+        NpyIter_Deallocate(iter);
+        return NULL;
+    }
+
+    // calculate prediction
+    do {
+        get_multi_index(iter, multi_index);
+        printf("multi_index is [%" NPY_INTP_FMT ", %" NPY_INTP_FMT "], %lf\n",
+               multi_index[0], multi_index[1], **dataptr);
+    } while (iternext(iter));
+
+    // update filter
+    char *reset_fail_msg = "Iterator reset failed";
+    if(NpyIter_Reset(iter, &reset_fail_msg) != NPY_SUCCEED) {
+        return NULL;
+    }
+    puts("");
+    do {
+        get_multi_index(iter, multi_index);
+        printf("multi_index is [%" NPY_INTP_FMT ", %" NPY_INTP_FMT "], %lf\n",
+               multi_index[0], multi_index[1], **dataptr);
+    } while (iternext(iter));
 
     // dealloc the iter instance
     if (!NpyIter_Deallocate(iter)) {
         return NULL;
     }
 
-    return PyFloat_FromDouble(self->filter->step());
+    return PyFloat_FromDouble(0.);
 }
 
 static PyMethodDef LMS_C_methods[] = {
@@ -159,7 +190,15 @@ LMS_C_init(LMS_C_OBJECT *self, PyObject *args, PyObject *kwds)
         return -1;
     }
 
-    self->filter = new LMSFilter(n_filter, idx_target, n_channel, step_scale, normalized);
+    self->n_filter = n_filter;
+    self->idx_target = idx_target;
+    self->n_channel = n_channel;
+    self->step_scale = step_scale;
+    self->normalized = normalized;
+
+    self->filter_coefficients.insert(self->filter_coefficients.begin(),
+            n_channel,
+            std::vector<double>(n_filter, 0));
 
     return 0;
 }
@@ -167,7 +206,6 @@ LMS_C_init(LMS_C_OBJECT *self, PyObject *args, PyObject *kwds)
 static void
 LMS_C_dealloc(LMS_C_OBJECT *self)
 {
-    delete self->filter;
     // this must call Py_XDECREF(object) for all held python objects
     Py_TYPE(self)->tp_free((PyObject *) self);
 }
