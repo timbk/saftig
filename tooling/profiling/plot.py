99ec326dc8a268902cd0000c68772e85ed434800
Tim Kuhlbusch <tim.kuhlbusch@rwth-aachen.de>
2025-07-11 11:35:53 +0200

    update profiling to include LMS filter in C

--- tooling/profiling/plot.py
+++ tooling/profiling/plot.py
@@ -3,22 +3,37 @@ import matplotlib.pyplot as plt
 from icecream import ic
 from glob import glob
 
+MARKER_MAPPING = {
+    'WF': 'o',
+    'UWF': '^',
+    'LMS': 'x',
+    'LMS_C': 'P',
+    'LMS_C': 'P',
+    'PolyLMS': 'v',
+}
+
 if __name__ == "__main__":
     for fname in glob('results/*.npz'):
         results = np.load(fname)
         ic(fname, results)
+        fname_core = '.'.join(fname.split('/')[-1].split('.')[:-1])
 
         target = results['target']
         target_values = results['target_values']
         data = results['results']
         other_values = results['other_values']
         filter_configs = results['filter_configs']
+        filter_names = results['filter_names']
         x_log = results['x_log'] if 'x_log' in results else True
         multithreaded = results['multithreaded']
-        
-        fig, ax = plt.subplots(2, sharex=True, sharey=True, figsize=(10, 6))
+
+        filter_types = dict()
+
+        fig, ax = plt.subplots(2, sharex=True, sharey=True, figsize=(10, 8))
         for idx_method, method in enumerate(['Conditioning', 'Application']):
-            ax[idx_method].plot(target_values, data[:,idx_method,:], marker='o', label=filter_configs)
+            markers = [MARKER_MAPPING[fn] for fn in filter_names]
+            for filter_name, dataset, label in zip(filter_names, data[:,idx_method,:].T, filter_configs):
+                ax[idx_method].plot(target_values, dataset, marker=MARKER_MAPPING[filter_name], label=label)
 
             ax[idx_method].set_ylabel(f'Processing rate\n{method} [Sps]')
             if x_log:
@@ -31,5 +46,8 @@ if __name__ == "__main__":
         p1 = ('Multi-thread' if multithreaded else 'Single-thread')
         p2 = ', '.join([f'{k}={v}' for k, v in other_values])
         ax[0].set_title(p1 + ' ' + p2)
+        plt.subplots_adjust(top=0.8)
 
-    plt.show()
+        plt.savefig(f'plots/{fname_core}.png')
+
+    #plt.show()
