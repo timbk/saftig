075f2f41f90290df9840b7140d3b011d66625814
Tim Kuhlbusch <tim.kuhlbusch@rwth-aachen.de>
2025-09-16 12:13:55 +0200

    Clan up the primary import statements

--- src/saftig/__init__.py
+++ src/saftig/__init__.py
@@ -2,12 +2,17 @@
 Implementations of prediction techniques with a unified interface.
 """
 
-from saftig import evaluation as eval  # pylint: disable=redefined-builtin
-from saftig import filtering as filt
+from saftig import evaluation
+from saftig import filtering
 from saftig import external
+
+eval = evaluation  # pylint: disable=redefined-builtin
+filt = filtering
 
 __all__ = [
     "eval",
     "filt",
     "external",
+    "evaluation",
+    "filtering",
 ]
